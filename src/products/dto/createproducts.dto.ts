6f678a1ab38255f1ef26b4a57801e5e59a559b63
chiwasushuba <joshua11084@gmail.com>
2025-10-31 20:09:12 +0800

    feat: added dtos for create and update

--- src/products/dto/createproducts.dto.ts
+++ src/products/dto/createproducts.dto.ts
@@ -1,7 +1,22 @@
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class CreateProductsDto {
+
+    @IsNumber()
+    @IsNotEmpty()
     product_category_id: number;
+
+    @IsNumber()
+    @IsNotEmpty()
     supplier_id: number;
+
+    @IsString()
+    @IsNotEmpty()
     product_name: string;
+
+    @IsString()
     product_description: string;
+
+    @IsBoolean()
     is_active: boolean;
 }
--- src/products/dto/updateprodcuts.dto.ts
+++ src/products/dto/updateprodcuts.dto.ts
@@ -1,5 +1,19 @@
-export class UpdateProductto {
-  name?: string;
-  price?: number;
-  description?: string;
+import { IsBoolean, IsNumber, IsString } from "class-validator";
+
+
+export class UpdateProductsDto {
+    @IsNumber()
+    product_category_id?: number;
+
+    @IsNumber()
+    supplier_id?: number;
+
+    @IsString()
+    product_name?: string;
+
+    @IsString()
+    product_description?: string;
+
+    @IsBoolean()
+    is_active?: boolean;
 }
