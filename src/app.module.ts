5ec5d932c0d44876664e12bc1719496b4de01701
chiwasushuba <joshua11084@gmail.com>
2025-10-31 19:19:20 +0800

    feat: init products and suppliers api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [SuppliersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
