11755f5e12fa6f50953c13f02db1240193506d20
chiwasushuba <joshua11084@gmail.com>
2025-11-01 23:43:43 +0800

    feat: added service and module for firebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SuppliersModule } from './suppliers/suppliers.module';
 import { ProductsModule } from './products/products.module';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
-  imports: [SuppliersModule, ProductsModule],
+  imports: [SuppliersModule, ProductsModule, FirebaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/firebase/firebase.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+
+@Injectable()
+export class FirebaseService implements OnModuleInit {
+  private firebaseApp: admin.app.App;
+
+  onModuleInit() {
+    if (!admin.apps.length) {
+      this.firebaseApp = admin.initializeApp({
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        }),
+      });
+    }
+  }
+
+  getAuth(): admin.auth.Auth {
+    return admin.auth(this.firebaseApp);
+  }
+}
