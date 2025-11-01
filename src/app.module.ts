5e3c6aacf638009447dffcfd0628f043776baf04
chiwasushuba <joshua11084@gmail.com>
2025-11-01 23:50:38 +0800

    feat: init user and auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { SuppliersModule } from './suppliers/suppliers.module';
 import { ProductsModule } from './products/products.module';
 import { FirebaseModule } from './firebase/firebase.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [SuppliersModule, ProductsModule, FirebaseModule],
+  imports: [SuppliersModule, ProductsModule, FirebaseModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
