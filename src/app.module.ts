25e560284579163556739f4e0de59dc13bce1bff
xandreiAThome <xandrei37@gmail.com>
2025-11-03 08:07:47 +0800

    fix: moved code under /featuers directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SuppliersModule } from './suppliers/suppliers.module';
-import { ProductsModule } from './products/products.module';
+import { SuppliersModule } from './features/suppliers/suppliers.module';
+import { ProductsModule } from './features/products/products.module';
 
 @Module({
   imports: [SuppliersModule, ProductsModule],
