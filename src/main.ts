aecdb231524ea29bb1cadf1347df8d70f60d563b
chiwasushuba <joshua11084@gmail.com>
2025-11-09 21:04:55 +0800

    feat: fixed dtos for product

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(process.env.PORT ?? 4000);
 }
