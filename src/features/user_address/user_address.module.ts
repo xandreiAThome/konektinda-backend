c33d163999ab6aa3d1963123952cceb6441f4bdb
chiwasushuba <joshua11084@gmail.com>
2025-11-24 22:35:24 +0800

    fix: fixed added firebaseModule import

--- src/features/user_address/user_address.module.ts
+++ src/features/user_address/user_address.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserAddressController } from './user_address.controller';
 import { UserAddressService } from './user_address.service';
 import { UsersModule } from '../users/users.module';
+import { FirebaseModule } from '../firebase/firebase.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, FirebaseModule],
   controllers: [UserAddressController],
   providers: [UserAddressService],
 })
