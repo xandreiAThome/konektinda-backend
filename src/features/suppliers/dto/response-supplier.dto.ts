ebcfd89b6296b956ed8a1ef0dfc5edd3b53a77bc
chiwasushuba <joshua11084@gmail.com>
2025-11-30 04:18:58 +0800

    fix: added docs in suppliers

--- src/features/suppliers/dto/response-supplier.dto.ts
+++ src/features/suppliers/dto/response-supplier.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class ResponseSupplierDto {
   @ApiProperty({ example: 1 })
+  @IsNumber()
   supplier_id: number;
 
   @ApiProperty({ example: 'Local Farm Co.' })
+  @IsString()
   supplier_name: string;
 
   @ApiPropertyOptional({ example: 'A trusted local supplier of fresh produce' })
+  @IsString()
   supplier_description: string | null;
 }
